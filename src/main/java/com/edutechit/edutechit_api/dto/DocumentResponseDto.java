438f648235c61c49965746e42a3e2e6fddac7b93
UserVVH <vuvanhaidt1@gmail.com>
2025-01-07 10:08:44 +0700

    change code BE

--- src/main/java/com/edutechit/edutechit_api/dto/DocumentResponseDto.java
+++ src/main/java/com/edutechit/edutechit_api/dto/DocumentResponseDto.java
@@ -3,33 +3,33 @@ package com.edutechit.edutechit_api.dto;
 import com.edutechit.edutechit_api.entity.Document;
 import com.edutechit.edutechit_api.util.TimeUtils;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Data;
-
 import java.time.LocalDateTime;
-import java.util.Set;
+import lombok.Data;
 
 @Data
 public class DocumentResponseDto {
-    private Long id;
-    private String title;
-    private String userName;
-    private String author;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
-    private LocalDateTime createdAt;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
-    private LocalDateTime updatedAt;
-    private String publisher;
-    private String publishingYear;
-    private String image;
-    private String categoryName;
-    private int view;
-    private Document.Status status;
 
-    public String getRelativeCreatedAt() {
-        return TimeUtils.getRelativeTime(createdAt);
-    }
+  private Long id;
+  private String title;
+  private String userName;
+  private String author;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+  private LocalDateTime createdAt;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+  private LocalDateTime updatedAt;
+  private String publisher;
+  private String publishingYear;
+  private String image;
+  private String categoryName;
+  private int view;
+  private Document.Status status;
 
-    public String getRelativeUpdatedAt() {
-        return TimeUtils.getRelativeTime(updatedAt);
-    }
+  //    Jackson nhận diện hai phương thức getRelativeCreatedAt và getRelativeUpdatedAt tự động thêm chúng vào JSON trả về
+  public String getRelativeCreatedAt() {
+    return TimeUtils.getRelativeTime(createdAt);
+  }
+
+  public String getRelativeUpdatedAt() {
+    return TimeUtils.getRelativeTime(updatedAt);
+  }
 }
